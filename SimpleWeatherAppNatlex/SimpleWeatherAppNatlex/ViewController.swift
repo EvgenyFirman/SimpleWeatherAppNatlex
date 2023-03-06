bdf0d305564e5f229a8a2c5e384a71d375eca9cc
Evgeny Firman <Eugeniolio@Yandex.ru>
2023-03-06 13:24:43 +0300

    Base Pod + gitignore

---
+++ SimpleWeatherAppNatlex/SimpleWeatherAppNatlex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleWeatherAppNatlex
+//
+//  Created by Евгений Фирман on 04.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SimpleWeatherAppNatlex/SimpleWeatherAppNatlex/WeatherMainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherMainViewController.swift
+//  SimpleWeatherAppNatlex
+//
+//  Created by Евгений Фирман on 06.03.2023.
+//
+
+import UIKit
+
+class WeatherMainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        view.backgroundColor = .red
+    }
+    
+}
